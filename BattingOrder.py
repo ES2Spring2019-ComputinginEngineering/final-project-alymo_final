460267cba75cd3f038a8b3b1989b0d1294a9b732
Mo <mo.liu@tufts.edu>
2019-04-17 15:26:13 -0400

    :)

--- BattingOrder.py
+++ BattingOrder.py
@@ -10,25 +10,24 @@ Created on Wed Apr 17 09:24:27 2019
 import numpy as np
 import csv
 
-def ReadDataFile():
-    csv_file = open('BattingLineUpData.csv')
+def read_data_file():
+    csv_file = open('ckd.csv')
     total_row = sum(1 for row in csv_file) - 1
     csv_file.seek(0)
-    csv_reader = csv.reader(csv_file, delimiter=',')     
-    global Blood_Glucose
-    Blood_Glucose = np.zeros((total_row,))
-    global Hemoglobin
-    Hemoglobin = np.zeros((total_row,))
-    Class = np.zeros((total_row,))
-    line_count = 0
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    batting_average = np.zeros((total_row,))
+    on_base = np.zeros((total_row,))
+    slugging = np.zeros((total_row,))
+    strike_out = np.zeros((total_row,))
+    stolen_bases = np.zeros((total_row,))
+    i = 0 
     for row in csv_reader:
-        if line_count == 0:
-            print(str(row))
+        if i == 0:
+            None
         else:
-            Blood_Glucose[line_count-1] = (int(row[0])-70)/420
-            Hemoglobin[line_count-1] = (float(row[1])-3.1)/14.7
-            Class[line_count-1] = int(row[2])
-        line_count += 1
-    print("Data Proccessed")
+            Glucose[i-1] = (int(row[0]) - 70)/420
+            Hemoglobin[i-1] = (float(row[1]) - 3.1)/14.7
+            Class[i-1] = int(row[2])
+        i += 1
     csv_file.close()
-    return Class, Hemoglobin, Blood_Glucose
+    return batting_average, on_base, slugging, strike_out
