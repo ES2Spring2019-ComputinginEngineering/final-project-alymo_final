4e4ec0fc948ac2acdad318dd75649d31aecaf6a0
Mo <mo.liu@tufts.edu>
2019-04-17 15:46:02 -0400

    :)

--- batting_order.py
+++ batting_order.py
@@ -11,10 +11,10 @@ import numpy as np
 import csv
 
 def read_data_file():
-    csv_file = open('ckd.csv')
+    csv_file = open("batting_line_up_data.csv")
     total_row = sum(1 for row in csv_file) - 1
     csv_file.seek(0)
-    csv_reader = csv.reader(csv_file, delimiter = ',')
+    csv_reader = csv.reader(csv_file, delimiter = ",")
     batting_average = np.zeros((total_row,))
     on_base = np.zeros((total_row,))
     slugging = np.zeros((total_row,))
@@ -25,9 +25,13 @@ def read_data_file():
         if i == 0:
             None
         else:
-            Glucose[i-1] = (int(row[0]) - 70)/420
-            Hemoglobin[i-1] = (float(row[1]) - 3.1)/14.7
-            Class[i-1] = int(row[2])
+            batting_average [i-1] = (float(row[1]) - .177)/.169
+            on_base [i-1] = (float(row[2]) - .232)/.206
+            slugging [i-1] = (float(row[3]) - .279)/.361
+            strike_out [i-1] = 1 - (float(row[4]) - .144)/.145
+            stolen_bases [i-1] = (float(row[5]) - 1)/29
         i += 1
     csv_file.close()
-    return batting_average, on_base, slugging, strike_out
+    return batting_average, on_base, slugging, strike_out, stolen_bases
+
+print(read_data_file())
