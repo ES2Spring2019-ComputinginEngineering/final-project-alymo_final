fd7868079d0395a7fedfe99dc66a120a9b3e3d0c
Mo <mo.liu@tufts.edu>
2019-04-23 17:35:11 -0400

    :)

--- game_simulation.py
+++ game_simulation.py
@@ -1,46 +1,82 @@
 import random
-from assigning_batters import find_batter_index
 
-def hit_quality(batter, batter_names, batting_average):
-    hit_quality = 0
-    batter_index = find_batter_index(batter, batter_names)
-    batter_average = batting_average[batter_index]
+def hit_quality(batter_average): #batter_average considers batting averagge, slugging, on_base, and strike out
+    bases_hit = 0
     for i in range(4):
         if batter_average > random.random():
-            hit_quality += 1
+            bases_hit += 1
         else:
             break
-    return hit_quality
+    return bases_hit
 
-def run_quality(batter, on_base, stolen_bases, next_occupied_base, hit_quality, current_base):
-    bases_run = min(next_occupied_base - current_base - 1, round(hit_quality * random.random() * stolen_bases * on_base))
-    return bases_run
+#def stealing(stealing_average): #runner_average considers 
+#    bases_stolen = 0 
+#        for i in range(4):
+#            if stealing_average > random.random():
+#                bases_stolen += 1
+#        return bases_stolen
 
-def score_lineup(lineup, batting_average, on_base, stolen_bases, hit_quality): 
+def single_lineup_score(on_base, batting_average, lineup_indices): 
+    runs = 0
+    current_bases = [0] * 4
+    for batter in lineup_indices:
+        batter_average = (on_base[batter] + batting_average[batter]) / 2
+        bases_hit = hit_quality(batter_average)
+        if bases_hit == 1:
+            current_bases = [1] + current_bases[0:3]
+            runs += int(current_bases[3])
+        if bases_hit == 2:
+            current_bases = [1] + current_bases[0:3]
+            runs += int(current_bases[3])
+            current_bases = [0] + current_bases[0:3]
+            runs += int(current_bases[3])
+        if bases_hit == 3:
+            current_bases = [1] + current_bases[0:3]
+            runs += int(current_bases[3])
+            current_bases = [0] + current_bases[0:3]
+            runs += int(current_bases[3])
+            current_bases = [0] + current_bases[0:3]
+            runs += int(current_bases[3])
+        if bases_hit == 4:
+            current_bases[3] = 0
+            runs += sum(current_bases) + 1
+            current_bases = [0] * 4
+    return runs
+    
+    
+def final_score(nlineup, on_base, batting_average, lineup_indices):
     total_runs = 0
-    ninnings = 9
-    for i in range(ninnings):
-        bases = [None] * 4
-        for batter in lineup:
-            strikes = 0
-            while strikes < 3:
-                hit_result = hit_quality(batter, batter_names, batting_average)
-                if hit_result == 0:
-                    strikes += 1
-                    continue 
-                else: 
-                    break
-                bases[0] = batter
-                next_occupied_base = 5
-                for i, runner in enumerate(reversed(bases)):
-                    current_base = 3 - i
-                    run = run_quality(runner, on_base, stolen_bases, next_occupied_base, hit_quality, current_base)
-                    if current_base + run == 4:
-                        total_runs += 1
-                        bases[current_base] = None
-                    else:
-                        bases[current_base + run] = bases[current_base]
-                        bases[current_base] = None
-                        next_occupied_base = current_base + run
+    for i in range(nlineup):
+        total_runs += single_lineup_score(on_base, batting_average, lineup_indices)
     return total_runs
 
+
+#    for i in range(ninnings):
+#        bases = [None] * 4
+#        nhits = hits(lineup)
+#        bases[0] = batter
+#        next_occupied_base = 5
+#        for i, runner in enumerate(reversed(bases)):
+#            current_base = 3 - i
+#            run = run_quality(runner, on_base, stolen_bases, next_occupied_base, hit_quality, current_base)
+#            if current_base + run == 4:
+#                total_runs += 1
+#                bases[current_base] = None
+#            else:
+#                bases[current_base + run] = bases[current_base]
+#                bases[current_base] = None
+#                next_occupied_base = current_base + run
+#    return total_runs
+
+
+#def hits(lineup):
+#     for batter in lineup:
+#            strikes = 0
+#            while strikes < 3:
+#                hit_result = hit_quality(batter, batter_names, batting_average)
+#                if hit_result == 0:
+#                    strikes += 1
+#                    continue 
+#                else: 
+#                    hits = hit_result
+#                    return hits
--- main.py
+++ main.py
@@ -3,19 +3,26 @@ make sure it runs correctly when someone downloads your repository. You
 might want to test this on a classmates computer to be sure it works!"""
 
 # This files should not contain any function defitions
-
-
 # IMPORT STATEMENTS
-from assigning_batters import read_stats, get_lineup, find_batter_index
-from game_simulation import score_lineup, hit_quality
+from assigning_batters import *
+from game_simulation import *
+import numpy as np
+import matplotlib.pyplot as plt
 
 # DEMONSTRATION CODE
+
+batting_average, on_base, slugging, strike_out, stolen_bases, batter_names = read_stats()
+
 lineup_indices, batter_names = get_lineup()
 print('Batting Lineup:')
 for i, batter_index in enumerate(lineup_indices):
     print(str(i+1) + ". " + batter_names[batter_index])
 
-batting_average, on_base, slugging, strike_out, stolen_bases, batter_names = read_stats()
-batter_index = find_batter_index("Sandy Leon", batter_names)
-hit_quality = hit_quality(batter_index, batter_names, batting_average[batter_index])
-print("score: " + str(score_lineup(lineup_indices, batting_average, on_base, stolen_bases, hit_quality)))
+score = np.zeros(50000)
+for i in range(50000):
+    score[i] = final_score(4, on_base, batting_average, lineup_indices)
+average_score = score.mean()
+print("\n Average Total Run: " + str(average_score))
+
+plt.hist(score, bins = np.linspace(0, 30, 31) + 0.5)
+plt.title("Final Score Distribution")
